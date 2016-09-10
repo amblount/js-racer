af6835a1d165a9d976da1cc0451128c317e01ad0
Alivia Blount <aliviablount@gmail.com>
2016-09-09 18:11:54 -0700

    get boxes moving

--- code/app.js
+++ code/app.js
@@ -1,22 +1,50 @@
 window.onload = function(){
   var player1 = document.getElementById('player1');
   var player2 = document.getElementById('player2');
+  var counta = 0;
+  var countb = 0;
 
-  // Move = add 10em to the position element to each position
+  //keycode 75 = k
+  //keycode 76 = l
 
-  player1.addEventListener("click", function(){
-    movePlayer1();
-  });
-  player2.addEventListener("click", function(){
-    movePlayer2();
-  });
+  document.addEventListener("keydown",test, false)
 
-  function movePlayer1(){
-    player1.style.left += 10%;
+  function test(event){
+    if (event.keyCode == 75){
+      console.log(player1.style);
+      // figure out how to get the left position style to work
+      counta += 10
+      player1.style.left = counta.toString() + "px";
+    };
+
+    if (event.keyCode == 76){
+      console.log(player2.style);
+      // figure out how to get the left position style to work
+      countb += 10
+      player2.style.left = countb.toString() + "px";
+    };
   };
 
-  function movePlayer2(){
-    player2.style.left += 10%;
-  };
+  // window.addEventListener("keyup", function(event) {
+  //   if (event.keyCode == 75)
+  //     document.body.style.background = "";
+  // });
+  //
+  // // Move = add 10em to the position element to each position
+  //
+  // player1.addEventListener("click", function(){
+  //   movePlayer1();
+  // });
+  // player2.addEventListener("click", function(){
+  //   movePlayer2();
+  // });
+  //
+  // function movePlayer1(){
+  //   player1.style.left += 10;
+  // };
+  //
+  // function movePlayer2(){
+  //   player2.style.left += 10;
+  // };
 
 };
